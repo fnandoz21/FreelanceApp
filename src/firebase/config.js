b4db544e3f78fd006245e24d460d4dc5e9f4cad8
Fernando Zapata <fzlakers1029@gmail.com>
2021-01-04 23:31:21 -0500

    initial commit

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+// import * as firebase from "firebase";
+import firebase from "firebase";
+import "@firebase/auth";
+import "@firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD4p9ETWhWYzi7fbZrfRDAlVherA1W8l2E",
+  authDomain: "danjones-fcc-tutorial.firebaseapp.com",
+  databaseURL: "https://danjones-fcc-tutorial-default-rtdb.firebaseio.com",
+  projectId: "danjones-fcc-tutorial",
+  storageBucket: "danjones-fcc-tutorial.appspot.com",
+  messagingSenderId: "150278363909",
+  appId: "1:150278363909:ios:556b2dd7543d12d719b56c",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export { firebase };
